de405568d37b66d33fbabe05da29b071b3606f5d
Piotr Miszczak <miszpiotr@wp.pl>
2020-11-16 21:33:22 +0100

    init commit

---
+++ server/data.js
@@ -0,0 +1,90 @@
+export default{
+    products:[
+  {
+    id: "1",
+    name: "Ms. Morris Cole",
+    avatar:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+    price: 68,
+    qty:12
+  },
+  {
+    id: "2",
+    name: "Etha Cummings",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/omnizya/128.jpg",
+    price: 83,
+  },
+  {
+    id: "3",
+    name: "America Ritchie",
+    avatar:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/happypeter1983/128.jpg",
+    price: 56,
+    qty:5
+  },
+  {
+    id: "4",
+    name: "Lilly Fahey",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/cboller1/128.jpg",
+    price: 87,
+    qty:15
+  },
+  {
+    id: "5",
+    name: "Eugene Bauch",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/ehsandiary/128.jpg",
+    price: 7,
+    qty:8
+  },
+  {
+    id: "6",
+    name: "Makayla Orn",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/psdesignuk/128.jpg",
+    price: 82,
+    qty:9
+  },
+  {
+    id: "7",
+    name: "Alfonso Mitchell",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/dgajjar/128.jpg",
+    price: 13,
+    qty:3
+  },
+  {
+    id: "8",
+    name: "Corbin Howe",
+    avatar:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/elbuscainfo/128.jpg",
+    price: 4,
+    qty:3
+  },
+  {
+    id: "9",
+    name: "Lois Rath",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/dreizle/128.jpg",
+    price: 36,
+    qty:5
+  },
+  {
+    id: "10",
+    name: "Vance Armstrong",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/begreative/128.jpg",
+    price: 44,
+    qty:8
+  },
+  {
+    id: "11",
+    name: "Jordy Heller",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/yehudab/128.jpg",
+    price: 35,
+    qty:13
+  },
+  {
+    id: "12",
+    name: "Wilma Monahan",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/instalox/128.jpg",
+    price: 37,
+    qty:7
+  },
+]}
+
---
+++ server/server.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import data from './data'
+import dotenv from 'dotenv'
+import config from './config'
+import mongoose from 'mongoose'
+import userRoute from '../routes/userRoute'
+
+dotenv.config();
+
+const MONGODBURL = config.MONGODB_URL;
+mongoose.connect(MONGODBURL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).catch(error => console.log('err2'))
+
+
+
+const app = express();
+
+app.use('/api/users',userRoute)
+
+app.get("/api/products", (req,res)=>{
+    res.send(data.products)
+});
+
+app.get("/products/api/products/:id", (req,res)=>{
+    const productId = req.params.id;
+    const product = data.products.find(x=>parseInt(x.id)==productId)
+   if(product){
+       res.send(product)}
+    else{
+        res.status(404).send({msg:'NOT xcxcFOUND'})
+    }
+});
+
+app.get("/api/products/:id", (req,res)=>{
+    const productId = req.params.id;
+    const product = data.products.find(x=>parseInt(x.id)==productId)
+   if(product){
+       res.send(product)}
+    else{
+        res.status(404).send({msg:'NOT xcxcFOUND'})
+    }
+});
+
+app.get("/cart/api/products/:id", (req,res)=>{
+    const productId = req.params.id;
+    const product = data.products.find(x=>parseInt(x.id)==productId)
+   if(product){
+       res.send(product)}
+    else{
+        res.status(404).send({msg:'NOT xcxcFOUND'})
+    }
+});
+
+app.listen(5000, ()=>console.log('Server started at http://localhost:5000'))
